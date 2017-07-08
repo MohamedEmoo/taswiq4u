ae1da546a34fae70b3f35bb86b9f8bf8fc0ee1e8
MohamedEmoo <mohamed_emam1994@yahoo.com>
2017-07-08 18:57:44 +0200

    eclipse rcp create new project wizard
    eclipse rcp create new project wizard

---
+++ taswiq4u/taswiq4u/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,25 @@
+﻿
+using taswiq4u.ViewModels;
+
+using Xamarin.Forms;
+
+namespace taswiq4u.Views
+{
+	public partial class ItemDetailPage : ContentPage
+	{
+		ItemDetailViewModel viewModel;
+
+        // Note - The Xamarin.Forms Previewer requires a default, parameterless constructor to render a page.
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+        }
+
+        public ItemDetailPage(ItemDetailViewModel viewModel)
+		{
+			InitializeComponent();
+
+			BindingContext = this.viewModel = viewModel;
+		}
+	}
+}
